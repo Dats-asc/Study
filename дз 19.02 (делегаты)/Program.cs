586a42ac26abec7aa6a0ac9aeb3d852920d20b52
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-03-02 20:47:12 +0300

    Add files via upload

---
+++ дз 19.02 (делегаты)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace дз_19._02
+{
+
+
+    class Program
+    {
+
+        delegate double Calculation(double r, ref string result);
+        
+        static double CircleLength(double r, ref string result)
+        {
+            result += $"Длина - {Math.Round(2 * Math.PI * r, 2)}, ";
+            return 2 * Math.PI * r;
+        }
+
+        static double CircleSquare(double r, ref string result)
+        {
+            result += $"площадь - {Math.Round(Math.PI * Math.Pow(r, 2),2)}, ";
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+        static double SphereVolume(double r, ref string result)
+        {
+            result += $"объём - {Math.Round(4.0 / 3 * Math.PI * Math.Pow(r, 3),2)}";
+            return 4.0 / 3 * Math.PI * Math.Pow(r, 3);
+        }
+
+        static void Main(string[] args)
+        {
+            Calculation circle = CircleLength;
+            circle += CircleSquare;
+            circle += SphereVolume;
+
+            string result = "";
+            double radius = double.Parse(Console.ReadLine());
+
+            circle(radius, ref result);
+            Console.WriteLine(result);
+        }
+    }
+}
