cb02b593e285d05b5a97d6eed6b7737d5e6a0260
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-03-02 21:22:24 +0300

    Add files via upload

---
+++ дз 19.02 (анонимный метод)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace дз_19._02
+{
+    class Program
+    {
+
+        delegate double Avarege(RandomNumbers[] numbers);
+
+        delegate int RandomNumbers();
+        
+        static int GetRandomNumber()
+        {
+            return new Random().Next();
+        }
+
+        static Avarege avarage = delegate (RandomNumbers[] numbers)
+        {
+            long summ = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                summ += numbers[i]();
+            }
+            return (double)summ / numbers.Length;
+        };
+
+        static void Main(string[] args)
+        {
+            Console.Write("Количество элементов массива - ");
+            int length = int.Parse(Console.ReadLine());
+
+            RandomNumbers[] numbers = new RandomNumbers[length];
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = GetRandomNumber;
+            }
+
+            Console.WriteLine($"Среднее арефметическое - {avarage(numbers)}");
+        }
+    }
+}
